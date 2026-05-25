c5f3793b8a2add450fb4f992052873a637a87a8b
alittleboyz <sanbomeng@gmail.com>
2026-05-25 15:26:23 +0800

    Update node.js

--- dashboard/admin/node.js
+++ dashboard/admin/node.js
@@ -2397,10 +2397,24 @@ function buildTxDesc(t){
 function emptyDataHTML(text = "No data"){
   return `
     <div class="emptyData">
-      <svg viewBox="0 0 24 24" aria-hidden="true">
-        <path fill="currentColor" d="M18.546 1h-13.069l-5.477 8.986v13.014h24v-13.014l-5.454-8.986zm-11.946 2h10.82l4.249 7h-5.083l-3 3h-3.172l-3-3h-5.08l4.266-7zm-4.6 18v-9h4.586l3 3h4.828l3-3h4.586v9h-20zm3-12l.607-1h12.786l.607 1h-14zm12.787-2h-11.572l.606-1h10.359l.607 1zm-1.215-2h-9.144l.607-1h7.931l.606 1z"/>
+      <svg width="64" height="41" viewBox="0 0 64 41" xmlns="http://www.w3.org/2000/svg">
+        <g transform="translate(0 1)" fill="none" fill-rule="evenodd">
+
+          <ellipse fill="#272727" cx="32" cy="33" rx="32" ry="7"></ellipse>
+
+          <g fill-rule="nonzero" stroke="#3e3e3e">
+            <path d="M55 12.76L44.854 1.258C44.367.474 43.656 0 42.907 0H21.093c-.749 0-1.46.474-1.947 1.257L9 12.761V22h46v-9.24z"></path>
+
+            <path
+              d="M41.613 15.931c0-1.605.994-2.93 2.227-2.931H55v18.137C55 33.26 53.68 35 52.05 35h-40.1C10.32 35 9 33.259 9 31.137V13h11.16c1.233 0 2.227 1.323 2.227 2.928v.022c0 1.605 1.005 2.901 2.237 2.901h14.752c1.232 0 2.237-1.308 2.237-2.913v-.007z"
+              fill="#1d1d1d">
+            </path>
+          </g>
+
+        </g>
       </svg>
-      <div>${escapeHtml(text)}</div>
+
+      <div class="emptyDataText">${escapeHtml(text)}</div>
     </div>
   `;
 }
